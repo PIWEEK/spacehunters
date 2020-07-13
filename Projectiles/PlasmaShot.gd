5c956db200076c81085d334ad6a68048109cd5ea
Juanfran <juanfran.ag@gmail.com>
2020-07-13 19:56:21 +0200

    plasma shot fade

--- Projectiles/PlasmaShot.gd
+++ Projectiles/PlasmaShot.gd
@@ -1,6 +1,8 @@
 extends KinematicBody2D
 
 onready var player := $AnimationPlayer
+onready var tween := $Tween
+onready var sprite := $Sprite
 
 export var speed := 1000.0
 export var damage := 10.0
@@ -12,6 +14,7 @@ var shooter: Node
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
     player.play("Flicker")
+    sprite.material = sprite.material.duplicate()
 
 func _physics_process(delta: float) -> void:
     var collision := move_and_collide(direction * speed * delta)
@@ -20,4 +23,13 @@ func _physics_process(delta: float) -> void:
        destroy()
 
 func destroy() -> void:
+    self.set_physics_process(false)
+    self.get_node("CollisionShape2D").disabled = true
+    
+    tween.interpolate_method(self, "_fade", 1.0, 0.0, 0.15, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+    tween.start()
+    yield(tween, "tween_all_completed")
     queue_free()
+
+func _fade(value: float) -> void:
+    sprite.material.set_shader_param("fade_amount", value)
