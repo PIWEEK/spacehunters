1627d4bdc8dbd8f31581c1afe8c1655a2591d8d1
Daniel Herrero García <daniel.herrero@kaleidos.net>
2020-07-14 14:14:47 +0200

    Change size of asteroids

--- Objects/MovingAsteroid.gd
+++ Objects/MovingAsteroid.gd
@@ -5,25 +5,37 @@ const VELOCITY = 3000
 const MIN_VELOCITY = 10
 const MAX_VELOCITY = 100 #20
 const ROTATION_DEGREES = 4
+const MIN_SCALE_FACTOR = 0.7
+const MAX_SCALE_FACTOR = 1.3
 
 var rnd = RandomNumberGenerator.new()
 var velocity = Vector2()
 
 func _ready():
     set_rotation_degrees(_random_between(0, 360))
-
-    velocity.y = _random_between(-VELOCITY, VELOCITY) # randomizes the x component of the velocity
-    velocity.x = _random_between(-VELOCITY, VELOCITY) # randomizes the y component of the velocity
-    velocity = velocity.normalized() * _random_between(MIN_VELOCITY,MAX_VELOCITY)
-
-    global_position.x = _random_between(-MAX_POSITION, MAX_POSITION)
-    global_position.y = _random_between(-MAX_POSITION, MAX_POSITION)
+    _set_random_velocity()
+    _set_random_position()
+    _set_random_size()
     
 func _process(delta):
     rotation_degrees += ROTATION_DEGREES * delta    
 
 func _physics_process(delta):
     move_and_collide(velocity * delta)
+
+func _set_random_position():
+    global_position.x = _random_between(-MAX_POSITION, MAX_POSITION)
+    global_position.y = _random_between(-MAX_POSITION, MAX_POSITION)
+
+func _set_random_velocity():
+    velocity.y = _random_between(-VELOCITY, VELOCITY) # randomizes the x component of the velocity
+    velocity.x = _random_between(-VELOCITY, VELOCITY) # randomizes the y component of the velocity
+    velocity = velocity.normalized() * _random_between(MIN_VELOCITY,MAX_VELOCITY)    
+
+func _set_random_size():
+    var _scaleFactor = _random_between(MIN_SCALE_FACTOR, MAX_SCALE_FACTOR)
+    var _scale = Vector2(_scaleFactor, _scaleFactor)
+    set_scale(_scale)
 
 func _random_between(minRnd, maxRnd):
     rnd.randomize()
