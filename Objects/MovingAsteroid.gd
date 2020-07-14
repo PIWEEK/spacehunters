f57e1059c913485326925c922ce6bc90cb2145ea
Daniel Herrero García <daniel.herrero@kaleidos.net>
2020-07-14 12:02:31 +0200

    Spawn periodically, rotate, and change velocity to asteroids

--- Objects/MovingAsteroid.gd
+++ Objects/MovingAsteroid.gd
@@ -1,25 +1,33 @@
 extends KinematicBody2D
 
-const SPEED = 20
-const MAX_POSITION = 1000
+const MAX_POSITION = 2500
+const VELOCITY = 3000
+const MIN_VELOCITY = 10
+const MAX_VELOCITY = 100 #20
+const ROTATION_DEGREES = 4
 
 var rnd = RandomNumberGenerator.new()
 var velocity = Vector2()
 
 func _ready():
-    rnd.randomize()
-    velocity.y = rnd.randf_range(-300, 300) # randomizes the x component of the velocity
-    rnd.randomize()
-    velocity.x = rnd.randf_range(-300, 300) # randomizes the y component of the velocity
-    velocity = velocity.normalized() * SPEED
+    set_rotation_degrees(_random_between(0, 360))
+
+    velocity.y = _random_between(-VELOCITY, VELOCITY) # randomizes the x component of the velocity
+    velocity.x = _random_between(-VELOCITY, VELOCITY) # randomizes the y component of the velocity
+    velocity = velocity.normalized() * _random_between(MIN_VELOCITY,MAX_VELOCITY)
+
+    global_position.x = _random_between(-MAX_POSITION, MAX_POSITION)
+    global_position.y = _random_between(-MAX_POSITION, MAX_POSITION)
     
-    rnd.randomize()
-    global_position.x = rnd.randf_range(-MAX_POSITION, MAX_POSITION)
-    rnd.randomize()
-    global_position.y = rnd.randf_range(-MAX_POSITION, MAX_POSITION)
-    position += velocity * 1
+func _process(delta):
+    rotation_degrees += ROTATION_DEGREES * delta    
 
 func _physics_process(delta):
     move_and_collide(velocity * delta)
-    move_and_collide(velocity * delta)
-    #position += velocity * delta
+
+func _random_between(minRnd, maxRnd):
+    rnd.randomize()
+    return rnd.randf_range(minRnd, maxRnd)
+    
+
+    
--- Objects/RandomAsteroids.gd
+++ Objects/RandomAsteroids.gd
@@ -1,14 +1,29 @@
 extends Node2D
 
-const ASTEROIDS_NUM = 15
+const ASTEROIDS_NUM = 40
 const MOVING_ASTEROID = preload("res://Objects/MovingAsteroid.tscn")
+const TIMER = 30.0 # seconds
+
+var _timer = null
 
 func _ready():
-    loadMovingAsteroids()
+    _spawn_asteroids()
+    _spawn_asteroids_periodically()
+    
+func _spawn_asteroids_periodically():
+    _timer = Timer.new()
+    add_child(_timer)
+    _timer.connect("timeout", self, "_on_Timer_timeout")
+    _timer.set_wait_time(TIMER)
+    _timer.set_one_shot(false) # Make sure it loops
+    _timer.start()
 
-func loadMovingAsteroids():
+func _spawn_asteroids():
     for i in range(ASTEROIDS_NUM):
         # create a new instance of an asteroid scene
         var asteroid = MOVING_ASTEROID.instance()
         # add it as a child of this "universe"-node       
         add_child(asteroid)
+
+func _on_Timer_timeout():
+    _spawn_asteroids()
