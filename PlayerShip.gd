d719d8aa6bb3ee00a6e2db0ce35af26aa5fcfa54
Juanfran <juanfran.ag@gmail.com>
2020-07-14 14:55:49 +0200

    boost

--- PlayerShip.gd
+++ PlayerShip.gd
@@ -2,13 +2,15 @@ extends KinematicBody2D
 
 onready var laser := $LaserBeam2D
 onready var network = get_node("/root/Network")
+var default_speed = 400
 
 var last_direction
-var speed = 400
+var speed = default_speed
 var turn_speed = deg2rad(5)
 var plasma = preload('res://Projectiles/PlasmaShot.tscn')
 var can_fire = true
 var fire_rate := 0.2
+var charge = false
 
 puppet var puppet_position = Vector2()
 puppet var puppet_direction = 0.0
@@ -24,7 +26,10 @@ func _ready() -> void:
         
     self.rotation = get_dir()
         
-func _process(delta: float) -> void:        
+func _process(delta: float) -> void:     
+    if charge:
+        return
+       
     if is_network_master():  
         if Input.is_action_pressed("fire_weapon") && not Input.is_action_pressed("fire_secondary_weapon") && can_fire:
             rpc("plasma_shot")
@@ -39,8 +44,17 @@ func get_dir():
     return self.get_angle_to(get_global_mouse_position())
     
 func _physics_process(delta: float) -> void:
+    if charge:
+        return
+
     var dir = get_dir()
     var movement = get_movement();
+    
+    if speed > default_speed:
+        speed -= delta * 300
+
+        if speed < default_speed:
+            speed = default_speed
     
     if is_network_master():   
         if last_direction != dir:     
@@ -71,12 +85,29 @@ func _physics_process(delta: float) -> void:
             queue_free()
     
 func get_movement() -> Vector2:
+    if speed > default_speed:
+        return Vector2(cos(self.rotation), sin(self.rotation))
+        
     return Vector2(
         Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
         Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
     )
 
+func init_charge():
+    charge =  true
+    $ParticlesCharge.emitting = true
+    yield(get_tree().create_timer(2.8), "timeout")
+    $ParticlesBoost.emitting = true    
+    charge =  false
+    speed = 3000
+
 func _unhandled_input(event: InputEvent) -> void:
+    if event.is_action("charge"):
+        
+        if event.is_action_pressed("charge"):
+            init_charge()
+        return
+    
     if is_network_master():
         if not event.is_action("fire_secondary_weapon"):
             return       
@@ -86,6 +117,10 @@ func _unhandled_input(event: InputEvent) -> void:
 
 remotesync func plasma_shot():
     var projectile = plasma.instance()
+    
+    if speed > default_speed:
+        projectile.speed = (speed - default_speed) + projectile.default_speed
+    
     projectile.global_position = $InitProjectile.global_position
     projectile.direction = Vector2(cos(self.rotation), sin(self.rotation))
     
