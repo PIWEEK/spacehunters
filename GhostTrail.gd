de994186b25554127bef2d726a0f1d1d6c301554
Daniel Herrero García <daniel.herrero@kaleidos.net>
2020-07-14 18:39:14 +0200

    Add ship trail

---
+++ GhostTrail.gd
@@ -0,0 +1,23 @@
+extends Sprite
+
+const TIMER = 0.5 # seconds
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    _fade_ship_timer()
+#    Tween.interpolate_propery(self, "modulate", Color(1,1,1,1), Color(1,1,1,0),.6, Tween.TRANS_SINE, Tween.EASE_OUT)
+#    Tween.start()
+
+func _fade_ship_timer():
+    var _timer = Timer.new()
+    add_child(_timer)
+    _timer.connect("timeout", self, "_on_Timer_timeout")
+    _timer.set_wait_time(TIMER)
+    _timer.set_one_shot(false) # Make sure it loops
+    _timer.start()
+
+func _on_Timer_timeout():
+    visible = false
+
+func _on_Tween_tween_completed(object, key):
+    queue_free()
