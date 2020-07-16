ae2f9ea6cb5aa2b644fba8fce7adb45c8a446982
Daniel Herrero García <daniel.herrero@kaleidos.net>
2020-07-16 13:51:45 +0200

    fix(zoom): Remove logs)

--- PlayerCamera.gd
+++ PlayerCamera.gd
@@ -6,11 +6,9 @@ const MIN_ZOOM = 0.5
 func zoom(delta):
     self.make_current()
     if Input.is_action_just_released("wheel_down"):
-        print(get_zoom())
         if zoom.x < MAX_ZOOM:  # set a maximum zoom level
             self.zoom += Vector2(.05, .05)
     if Input.is_action_just_released('wheel_up'):
-        print(get_zoom())
         if zoom.x > MIN_ZOOM:  # set a maximum zoom level
             self.zoom -= Vector2(5, 5) * delta  # add to the zoom every frame
 
